2bf1b2ec81fb8e8fdafaf5c9fcebbe6767f1163a
Eugene Kiselev <jkiselev2411@gmail.com>
2024-08-23 11:38:04 +0600

    fix flashlight project
    add cross-browser support

--- flashlight/app.js
+++ flashlight/app.js
@@ -1,12 +1,17 @@
 const $container = document.querySelector(".container");
 const $flashlight = document.querySelector(".flashlight");
 
+const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
+
 let flashlightRadius = 50;
 
 $container.addEventListener("mousemove", (event) => {
   $flashlight.style.display = "block";
   $flashlight.style.top = event.offsetY + "px";
   $flashlight.style.left = event.offsetX + "px";
+
+  if (!isFirefox)
+    $flashlight.style.border = `${flashlightRadius}px solid white`;
 });
 
 $container.addEventListener("mouseleave", () => {
@@ -17,11 +22,13 @@ $container.addEventListener("wheel", (event) => {
   event.preventDefault();
 
   if (event.deltaY < 0) {
-    flashlightRadius = Math.min(60, flashlightRadius + 10);
+    flashlightRadius = Math.min(90, flashlightRadius + 10);
   } else {
-    flashlightRadius = Math.max(30, flashlightRadius - 10);
+    flashlightRadius = Math.max(50, flashlightRadius - 10);
   }
   $flashlight.style.boxShadow = `0px 0px 20px ${flashlightRadius}px white`;
-  $flashlight.style.border = `${flashlightRadius}px solid white`;
   $flashlight.style.borderRadius = `${flashlightRadius}px`;
+
+  if (!isFirefox)
+    $flashlight.style.border = `${flashlightRadius}px solid white`;
 });
