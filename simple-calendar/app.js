9666f23aa15d462763d0e0c0c6d88acca00b9b44
Eugene Kiselev <jkiselev2411@gmail.com>
2025-04-08 17:48:56 +0600

    updated project simple calendar

--- simple-calendar/app.js
+++ simple-calendar/app.js
@@ -74,11 +74,39 @@ function getEngLang() {
     "[weekday-short-6]": "Sat",
     "[weekday-short-7]": "Sun",
 
-    "[month-4]": "April",
-
-    "[month-short-4]": "Apr",
-
     "[weekday-1]": "Monday",
+    "[weekday-2]": "Tuesday",
+    "[weekday-3]": "Wednesday",
+    "[weekday-4]": "Thursday",
+    "[weekday-5]": "Friday",
+    "[weekday-6]": "Saturday",
+    "[weekday-7]": "Sunday",
+
+    "[month-1]": "January",
+    "[month-2]": "February",
+    "[month-3]": "March",
+    "[month-4]": "April",
+    "[month-5]": "May",
+    "[month-6]": "June",
+    "[month-7]": "July",
+    "[month-8]": "August",
+    "[month-9]": "September",
+    "[month-10]": "October",
+    "[month-11]": "November",
+    "[month-12]": "December",
+
+    "[month-short-1]": "Jan",
+    "[month-short-2]": "Feb",
+    "[month-short-3]": "Mar",
+    "[month-short-4]": "Apr",
+    "[month-short-5]": "May",
+    "[month-short-6]": "Jun",
+    "[month-short-7]": "Jul",
+    "[month-short-8]": "Aug",
+    "[month-short-9]": "Sep",
+    "[month-short-10]": "Oct",
+    "[month-short-11]": "Nov",
+    "[month-short-12]": "Dec",
   };
   return lang;
 }
@@ -93,11 +121,39 @@ function getRusLang() {
     "[weekday-short-6]": "Сб",
     "[weekday-short-7]": "Вс",
 
-    "[month-4]": "Апрель",
-
-    "[month-short-4]": "Апр",
-
     "[weekday-1]": "Понедельник",
+    "[weekday-2]": "Вторник",
+    "[weekday-3]": "Среда",
+    "[weekday-4]": "Четверг",
+    "[weekday-5]": "Пятница",
+    "[weekday-6]": "Суббота",
+    "[weekday-7]": "Воскресенье",
+
+    "[month-1]": "Январь",
+    "[month-2]": "Февраль",
+    "[month-3]": "Март",
+    "[month-4]": "Апрель",
+    "[month-5]": "Май",
+    "[month-6]": "Июнь",
+    "[month-7]": "Июль",
+    "[month-8]": "Август",
+    "[month-9]": "Сентябрь",
+    "[month-10]": "Октябрь",
+    "[month-11]": "Ноябрь",
+    "[month-12]": "Декабрь",
+
+    "[month-short-1]": "Янв",
+    "[month-short-2]": "Фев",
+    "[month-short-3]": "Мар",
+    "[month-short-4]": "Апр",
+    "[month-short-5]": "Май",
+    "[month-short-6]": "Июн",
+    "[month-short-7]": "Июл",
+    "[month-short-8]": "Авг",
+    "[month-short-9]": "Сен",
+    "[month-short-10]": "Окт",
+    "[month-short-11]": "Ноя",
+    "[month-short-12]": "Дек",
   };
   return lang;
 }
@@ -112,11 +168,39 @@ function getKgzLang() {
     "[weekday-short-6]": "Ише",
     "[weekday-short-7]": "Жек",
 
-    "[month-4]": "Чын Куран",
-
-    "[month-short-4]": "чын куран",
-
     "[weekday-1]": "Дүйшөмбү",
+    "[weekday-2]": "Шейшемби",
+    "[weekday-3]": "Шаршемби",
+    "[weekday-4]": "Бейшемби",
+    "[weekday-5]": "Жума",
+    "[weekday-6]": "Ишемби",
+    "[weekday-7]": "Жекшемби",
+
+    "[month-1]": "Үчтүн айы",
+    "[month-2]": "Бирдин айы",
+    "[month-3]": "Жалган Куран",
+    "[month-4]": "Чын Куран",
+    "[month-5]": "Бугу",
+    "[month-6]": "Кулжа",
+    "[month-7]": "Теке",
+    "[month-8]": "Баш Оона",
+    "[month-9]": "Аяк Оона",
+    "[month-10]": "Тогуздун айы",
+    "[month-11]": "Жетинин айы",
+    "[month-12]": "Бештин айы",
+
+    "[month-short-1]": "Үчтүн айы",
+    "[month-short-2]": "Бирдин айы",
+    "[month-short-3]": "Жалган Куран",
+    "[month-short-4]": "Чын Куран",
+    "[month-short-5]": "Бугу",
+    "[month-short-6]": "Кулжа",
+    "[month-short-7]": "Теке",
+    "[month-short-8]": "Баш Оона",
+    "[month-short-9]": "Аяк Оона",
+    "[month-short-10]": "Тогуздун айы",
+    "[month-short-11]": "Жетинин айы",
+    "[month-short-12]": "Бештин айы",
   };
   return lang;
 }
