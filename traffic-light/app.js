3c084c0684ec349053de3717402a1b163d9daee7
alterradeveloper <jkiselev2411@gmail.com>
2026-04-30 21:54:10 +0600

    project traffic-light: added hover handler

--- traffic-light/app.js
+++ traffic-light/app.js
@@ -1,5 +1,3 @@
-
-
 const $redBtn = document.querySelector("#red");
 const $yellowBtn = document.querySelector("#yellow");
 const $greenBtn = document.querySelector("#green");
@@ -9,29 +7,54 @@ const $action = document.querySelector(".action");
 const defaultColor = "#444";
 
 $redBtn.onclick = () => {
-    $redBtn.style.backgroundColor = "red";
-    $yellowBtn.style.backgroundColor = defaultColor;
-    $greenBtn.style.backgroundColor = defaultColor;
-    
-    $action.textContent = "stop";
-    $action.style.color = "red";
-}
+  $redBtn.style.backgroundColor = "red";
+  $yellowBtn.style.backgroundColor = defaultColor;
+  $greenBtn.style.backgroundColor = defaultColor;
+
+  $action.textContent = "stop";
+  $action.style.color = "red";
+};
 
 $yellowBtn.onclick = () => {
-    $redBtn.style.backgroundColor = defaultColor;
-    $yellowBtn.style.backgroundColor = "#d9d907";
-    $greenBtn.style.backgroundColor = defaultColor;
-    
-    $action.textContent = "wait";
-    $action.style.color = "#d9d907";
-}
+  $redBtn.style.backgroundColor = defaultColor;
+  $yellowBtn.style.backgroundColor = "#d9d907";
+  $greenBtn.style.backgroundColor = defaultColor;
+
+  $action.textContent = "wait";
+  $action.style.color = "#d9d907";
+};
 
 $greenBtn.onclick = () => {
-    $redBtn.style.backgroundColor = defaultColor;
-    $yellowBtn.style.backgroundColor = defaultColor;
-    $greenBtn.style.backgroundColor = "#07a607";
-    
-    $action.textContent = "go";
-    $action.style.color = "#07a607";
-}
+  $redBtn.style.backgroundColor = defaultColor;
+  $yellowBtn.style.backgroundColor = defaultColor;
+  $greenBtn.style.backgroundColor = "#07a607";
 
+  $action.textContent = "go";
+  $action.style.color = "#07a607";
+};
+
+$redBtn.onmouseenter = () => {
+  $redBtn.style.backgroundColor = "red";
+  $action.textContent = "stop";
+  $action.style.color = "red";
+};
+
+$yellowBtn.onmouseenter = () => {
+  $yellowBtn.style.backgroundColor = "yellow";
+  $action.textContent = "wait";
+  $action.style.color = "yellow";
+};
+
+$greenBtn.onmouseenter = () => {
+  $greenBtn.style.backgroundColor = "green";
+  $action.textContent = "go";
+  $action.style.color = "green";
+};
+
+[$redBtn, $yellowBtn, $greenBtn].forEach((btn) => {
+  btn.onmouseleave = () => {
+    btn.style.backgroundColor = defaultColor;
+    $action.textContent = "";
+    $action.style.color = "black";
+  };
+});
