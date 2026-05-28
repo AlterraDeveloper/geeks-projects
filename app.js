0ad45cd31ea36164e4b8e264cd051920fbdd5eae
Evgenii Dmitrievich (Kiselev) <e.kiselev@umai.kg>
2026-05-28 11:26:45 +0600

    Added copy button for homeworks (humster-light)

--- app.js
+++ app.js
@@ -3,7 +3,19 @@
 const projectBlocks = document.querySelectorAll(".project");
 
 projectBlocks.forEach((block) => {
-  block.addEventListener("click", () => {
+  block.addEventListener("click", (event) => {
+
+    if (event.target.classList.contains("copy-btn")) {
+      const projectName = block.dataset.projectName;
+      const text = projectsInfo[projectName]?.text || "No description available.";
+      window.navigator.clipboard.writeText(text).then(() => {
+        // alert(`Project name "${projectName}" copied to clipboard!`);
+      }).catch((err) => {
+        console.error("Failed to copy project name: ", err);
+      });
+      return;
+    } 
+
     const projectName = block.dataset.projectName;
     const mainUrl = location.origin;
     const isLocalhost = location.hostname === "localhost" || location.hostname === "127.0.0.1";
